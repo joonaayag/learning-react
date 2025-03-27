adee38f916ba6920c6d655f5c9baf16b6dcd401b
Jonay Aguilar Menendez <joonaayag19@gmail.com>
2025-03-27 12:35:35 +0100

    End of the small contact with react

--- projects/00-hello-world/src/App.jsx
+++ projects/00-hello-world/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { TwitterFollowCarrd } from "./TwitterFollowCarrd";
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+    {
+        userName: "midudev",
+        name: "Miguel Ángel Durán",
+        initialIsFollowing: true
+    },
+    {
+        userName: "pheralb",
+        name: "Pablo Hernandez",
+        initialIsFollowing: false
+    },
+    {
+        userName: "elonmusk",
+        name: "Elon Musk",
+        initialIsFollowing: false
+    }
+]
 
+export function App() {
+    const formatUserName = (userName) => `@${userName}`
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="App">
+
+    {
+        users.map(user => {
+            const {userName, name, initialIsFollowing} = user;
+            return <TwitterFollowCarrd key={userName} formatUserName={formatUserName} userName={userName} name={name} initialIsFollowing={initialIsFollowing} />
+        })
+    }      
+      
+    </section>
   )
 }
-
-export default App
---
+++ projects/00-hello-world/src/TwitterFollowCarrd.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+export function TwitterFollowCarrd ({formatUserName, userName = 'Unknown', name = 'Unknown', initialIsFollowing}) {
+    //State returns 2 values, the first one is the value of the state and the second one is the function to update the state
+    // const state = useState(false);
+    // const isFollowing = state[0];
+    // const setisFollowing = state[1];
+    // ==
+    const [isFollowing, setisFollowing] = useState(initialIsFollowing);
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    const srcImage = `https://unavatar.io/${userName}`
+
+    const handleClick = () => {
+        setisFollowing(!isFollowing)
+    }
+    
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={srcImage} alt="Avatar de miduddev" />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                </div>
+            </header>
+            <aside>
+                <button onClick={handleClick} className={buttonClassName}>
+                    <span className="tw-followCard-text">{text}</span>
+                <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
