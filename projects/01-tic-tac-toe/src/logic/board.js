949477c2226c15b20e0c262cda76c5b3fe8635e7
Jonay Aguilar Menendez <joonaayag19@gmail.com>
2025-03-27 17:34:45 +0100

    Add 3 project

--- projects/01-tic-tac-toe/src/logic/board.js
+++ projects/01-tic-tac-toe/src/logic/board.js
@@ -1,23 +1,21 @@
-import { WINNER_COMBOS } from "../constants.js"
+import { WINNER_COMBOS } from '../constants.js'
 export const checkWinnerFrom = (boardToCheck) => {
-   for(const combo of WINNER_COMBOS){
-     const [a, b, c] = combo
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
     // Check if there is a winner
-     if (
+    if (
       boardToCheck[a] &&
       boardToCheck[a] === boardToCheck[b] &&
       boardToCheck[a] === boardToCheck[c]
-     ) {
+    ) {
       return boardToCheck[a]
-     }  
+    }
+  }
 
-   }
-
-   //No winner
-   return null
-
+  // No winner
+  return null
 }
 
 export const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
+  return newBoard.every((square) => square !== null)
+}
--- projects/01-tic-tac-toe/src/logic/storage/index.js
+++ projects/01-tic-tac-toe/src/logic/storage/index.js
@@ -1,10 +1,10 @@
-export const saveGameToStorage = ({board, turn}) => {
-      //save the move
-      window.localStorage.setItem('board', JSON.stringify(board))
-      window.localStorage.setItem('turn', JSON.stringify(turn))
+export const saveGameToStorage = ({ board, turn }) => {
+  // save the move
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', JSON.stringify(turn))
 }
 
 export const resetGameStorage = () => {
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
 }
